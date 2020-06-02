fd24b52553e4874bac420f008b9d0cac0cc2d6c1
msegnit <melsegnit@gmail.com>
2020-06-02 10:27:13 -0500

    Updated Component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
-
-    return <div>Hi There!</div>;
-};
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => console.log(position),
+            err => console.log(err)
+        );
+        return <div>Latitude: </div>;
+    }
+} 
 
 ReactDOM.render(
     <App />, 
